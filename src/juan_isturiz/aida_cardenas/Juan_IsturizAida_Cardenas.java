a12445418c10fc869e7ae49451382c3e87081892
yunch <yunch@Juan-Isturiz>
2021-03-19 09:01:51 -0400

    creado

---
+++ src/juan_isturiz/aida_cardenas/Juan_IsturizAida_Cardenas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juan_isturiz.aida_cardenas;
+
+/**
+ *
+ * @author yunch
+ */
+public class Juan_IsturizAida_Cardenas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
